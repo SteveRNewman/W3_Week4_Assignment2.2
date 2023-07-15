2a41104adc855bc2e74c635545be8052109c0cd2
SteveNewman <stevenewmanphotography@gmail.com>
2023-07-15 15:53:13 -0400

    updated leap year NOT % 400

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ let year = 2024; // not
 
 //Add code here
 
-if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+if (year % 4 == 0 && year % 100 == 0 && year % 400 !== 0) {
   console.log(`${year} is a leap year.`);
 } else {
   console.log(`${year} is NOT a leap year.`);
